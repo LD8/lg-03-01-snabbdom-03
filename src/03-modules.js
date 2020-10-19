f64a005d7f4cbdf719e9155b05c262b6add31814
lipeiwen <lipeiwen@chaintor.com>
2020-10-19 10:03:20 +0800

    feat: add 03-module

--- src/03-modules.js
+++ src/03-modules.js
@@ -1,34 +1,34 @@
 /**
  * 模块的使用
  */
-import { h } from 'snabbdom/build/package/h'
-import { init } from 'snabbdom/build/package/init'
+import { h } from "snabbdom/build/package/h";
+import { init } from "snabbdom/build/package/init";
 // 导入模块
-import { styleModule } from 'snabbdom/build/package/modules/style'
-import { eventListenersModule } from 'snabbdom/build/package/modules/eventlisteners'
+import { styleModule } from "snabbdom/build/package/modules/style";
+import { eventListenersModule } from "snabbdom/build/package/modules/eventlisteners";
 
 // 注册模块
-const patch = init([styleModule, eventListenersModule])
+const patch = init([styleModule, eventListenersModule]);
 
 // 传入模块需要的数据（对象）
 let vnode = h(
-  'div#container',
+  "div#container",
   {
     style: {
-      backgroundColor: 'grey',
+      backgroundColor: "grey"
     },
     on: {
       click() {
-        alert('clicked')
-      },
-    },
+        alert("clicked");
+      }
+    }
   },
   [
-    h('h1#h1', 'h1 Node'),
-    h('p#p', 'p Node', [h('span', 'span1 '), h('span', 'span2')]),
+    h("h1#h1", "h1 Node"),
+    h("p#p", "p Node", [h("span", "span1 "), h("span", "span2")])
   ]
-)
+);
 // console.log(vnode)
-const app = document.querySelector('#app')
+const app = document.querySelector("#app");
 
-let oldVNode = patch(app, vnode)
+let oldVNode = patch(app, vnode);
